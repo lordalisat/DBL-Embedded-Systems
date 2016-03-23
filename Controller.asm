853a357585024221373418b22ef3d728f162462e
lordalisat <b.wieringa31@gmail.com>
2016-03-23 15:48:54 +0100

    Changed light switch test

--- Controller.asm
+++ Controller.asm
@@ -1,31 +1,31 @@
 @DATA
-					; colorDet 168 for black
-	 CODE		DS	1	; variable where CODE location is saved
-       BUTBUF		DW	0	; the previous button input
-       CURBUT		DW	0	;
-      STOPBUF		DW	0	; the previous stop button input
-      STOPBUT		DW	0	;
-	DELTA		DW	10	; value for timer delay
-	 STEP		DW	0	; current step, for PWM
-	  PWM		DW	40	; max value for PWM
-	MOTOR		DS	1	; variable for motor state
-	STATE		DS	3	; variable for state ColorLight:ProxLight1:ProxLight2
-       PAUSED		DW	1	;
-        ABORT		DW	0	;
-       ETIMER		DW	0	; variable for the empty detector
-        EMPTY		DW	1	; boolean for empty
+						; colorDet 168 for black
+	 CODE		DS	1		; variable where CODE location is saved
+       BUTBUF		DW	0		; the previous button input
+       CURBUT		DW	0		;
+      STOPBUF		DW	0		; the previous stop button input
+      STOPBUT		DW	0		;
+	DELTA		DW	10		; value for timer delay
+	 STEP		DW	0		; current step, for PWM
+	  PWM		DW	40		; max value for PWM
+	MOTOR		DS	1		; variable for motor state
+	STATE		DS	3		; variable for state ColorLight:ProxLight1:ProxLight2
+       PAUSED		DW	1		;
+        ABORT		DW	0		;
+       ETIMER		DW	0		; variable for the empty detector
+        EMPTY		DW	1		; boolean for empty
 
 
 @CODE
 
-	IO_AREA		EQU	-16	; base address of the I/O-Area
-	TIMER		EQU	13	; timer register
-	OUTPUT		EQU	11	; the 8 outputs
-	LEDS		EQU	10	; the 3 LEDs above the 3 slide switches
-	DSPDIG		EQU	9	; register selecting the active display element
-	DSPSEG		EQU	8	; register for the pattern on the active display element
-	INPUT		EQU	7	; the 8 inputs
-	ADCONVS		EQU	6	; the outputs, concatenated, of the 2 A/D-converters
+	IO_AREA		EQU	-16		; base address of the I/O-Area
+	TIMER		EQU	13		; timer register
+	OUTPUT		EQU	11		; the 8 outputs
+	LEDS		EQU	10		; the 3 LEDs above the 3 slide switches
+	DSPDIG		EQU	9		; register selecting the active display element
+	DSPSEG		EQU	8		; register for the pattern on the active display element
+	INPUT		EQU	7		; the 8 inputs
+	ADCONVS		EQU	6		; the outputs, concatenated, of the 2 A/D-converters
 	
 	; Button and Detector values
 	START		EQU	%000000001	; location of the Start/Stop button
@@ -43,7 +43,10 @@
      MOTORCCW		EQU	%001
      MOTOROFF		EQU	%000
      
-        ETIME		EQU	165		; 6 ms marge van 1590
+     	   ON		EQU	%01
+     	  OFF		EQU	%00
+      
+        ETIME		EQU	170		; 11 ms marge van 1590
 	
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;	Timer Interrupt Enable	;
@@ -148,7 +151,8 @@ Idle:
 IdlePausedInit:
 	LOAD		R4		[GB+ABORT]
 	 BNE		Abort
-	 BRS		EmptyCheck
+	 BRS		LightSwitch
+	
 	LOAD		R4		[GB+EMPTY]
 	 BEQ		IdlePaused
 	LOAD		R2		MOTORCW
@@ -156,6 +160,8 @@ IdlePausedInit:
 	 BRA		ToIdle1
 	 
 IdlePaused:
+	LOAD		R4		[GB+ABORT]
+	 BNE		Abort
 	 BRS		StopButtonCheck
 	LOAD		R1		[GB+STOPBUT]
 	 AND		R1		START
@@ -169,27 +175,27 @@ Scanning:
 ;	Additional Checks	;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-EmptyCheck:
+LightSwitch:
 	LOAD		R4		[GB+ABORT]
-	 BNE		Abort
+	 BNE		LightSwitchDone
 	LOAD		R0		ETIME
 	STOR		R0		[GB+ETIMER]
-	LOAD		R1		%100
-	STOR		R1		[GB+STATE]
+	LOAD		R1		ON
+	STOR		R1		[GB+STATE+2]
 	
-EmptyCheckLoop:
+LightSwitchLoop:
 	LOAD		R4		[GB+ABORT]
-	 BNE		Abort
+	 BNE		LightSwitchDone
 	LOAD		R0		[GB+ETIMER]
-	 BNE		EmptyCheckLoop
+	 BNE		LightSwitchLoop
 	 
-EmptyCheckFinish:
+LightSwitchFinish:
 	LOAD		R4		[GB+ABORT]
-	 BNE		Abort
-	LOAD		R2		[R5+OUTPUT]
-	 AND		R2		PROXE
-	 MOD		R2		PROXE
-	STOR		R2		[GB+EMPTY]
+	 BNE		LightSwitchDone
+	LOAD		R1		OFF
+	STOR		R1		[GB+STATE+2]
+	
+LightSwitchDone:
 	 RTS
 	 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
